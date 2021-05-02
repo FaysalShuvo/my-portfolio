2ecd7ccc5a0346a54d0b01e15d9b59e5c9f7662a
Faysal Shuvo <32492440+FaysalShuvo@users.noreply.github.com>
2021-05-02 15:29:05 +0600

    upae

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Blogs.css";
 import { css } from "@emotion/core";
 import ClockLoader from "react-spinners/ClockLoader";
 
 const Blogs = () => {
-  let [loading, setLoading] = useState(true);
-  setLoading(true);
+  let [loading, setLoading] = useState();
+
+  useEffect(() => {
+    setLoading(true);
+  }, []);
   const override = css`
     margin: 0 auto;
     border-color: red;
